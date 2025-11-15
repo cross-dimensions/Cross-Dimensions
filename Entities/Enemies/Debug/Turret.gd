e5ba050e6b42fa863d37992c6f1cef90b7e9babc
TheImagineer0 <43724601+TheImagineer0@users.noreply.github.com>
2025-11-15 01:08:56 -0800

    Added bullets to turret

--- Entities/Enemies/Debug/Turret.gd
+++ Entities/Enemies/Debug/Turret.gd
@@ -1,16 +1,12 @@
 class_name Turret extends Node2D
 
 @onready var turretAxis = $TurretAxis
-<<<<<<< HEAD
 @onready var bullet = preload("res://Entities/Enemies/Debug/Bullet.tscn")
 @onready var bulletSpawnPoint = $TurretAxis/Turret/BulletSpawn
 
 var reachPlayer : bool = false
 var axisAngle
 
-=======
-
->>>>>>> a505d8e (Turret rotates to player position, also player is in global player group)
 var player
 
 # Called when the node enters the scene tree for the first time.
@@ -20,23 +16,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# Get players angle relative to turret axis
-<<<<<<< HEAD
 	axisAngle = self.get_angle_to(player.global_position) - deg_to_rad(90)
 	
 	if axisAngle > deg_to_rad(60) or axisAngle < deg_to_rad(-60):
 		reachPlayer = false
 	else:
 		reachPlayer = true
-=======
-	var axisAngle = self.get_angle_to(player.global_position) - deg_to_rad(90)
->>>>>>> a505d8e (Turret rotates to player position, also player is in global player group)
 	
 	# Constrain the rotation of the axis
 	axisAngle = clamp(axisAngle, deg_to_rad(-60), deg_to_rad(60))
 	
 	# Set rotation
 	turretAxis.set_rotation(axisAngle)
-<<<<<<< HEAD
 
 
 func _on_bullet_timer_timeout() -> void:
@@ -48,5 +39,3 @@ func _on_bullet_timer_timeout() -> void:
 		add_child(bulletChild)
 		
 		bulletChild.global_position = bulletSpawnPoint.global_position
-=======
->>>>>>> a505d8e (Turret rotates to player position, also player is in global player group)
