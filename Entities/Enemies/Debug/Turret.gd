a505d8eff7ce72de7a578619ea3aca6b23fa9a95
TheImagineer0 <43724601+TheImagineer0@users.noreply.github.com>
2025-11-15 00:40:57 -0800

    Turret rotates to player position, also player is in global player group

--- Entities/Enemies/Debug/Turret.gd
+++ Entities/Enemies/Debug/Turret.gd
@@ -1,13 +1,20 @@
 class_name Turret extends Node2D
 
+@onready var turretAxis = $TurretAxis
 
-
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	player = get_tree().get_first_node_in_group("Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	# Get players angle relative to turret axis
+	var axisAngle = self.get_angle_to(player.global_position) - deg_to_rad(90)
+	
+	# Constrain the rotation of the axis
+	axisAngle = clamp(axisAngle, deg_to_rad(-60), deg_to_rad(60))
+	
+	# Set rotation
+	turretAxis.set_rotation(axisAngle)
