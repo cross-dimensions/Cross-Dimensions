a552a4649b749dc51ec4a2040709fb261107a487
magicalfeyfenny <magicalfeyfenny@fenny.moe>
2025-11-18 11:25:26 -0800

    create classes
    created classes for buttons and doors
    
    updated add_movement.gd to have all tabs instead of mixed tabs/spaces since godot is giving error

---
+++ Entities/Environment/Debug/SwitchButton.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+namespace CrossDimensions.Environment.Puzzles;
+
+public partial class SwitchButton : Node
+{
+    public bool SwitchPressed = false;
+
+    
+
+
+}
---
+++ Entities/Environment/Debug/SwitchDoor.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+namespace CrossDimensions.Environment.Puzzles;
+
+public partial class SwitchDoor : Node
+{
+    public SwitchButton Switches;
+
+    public void Activate()
+    {
+        
+    }
+}
