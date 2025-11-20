9e04adae8c6c3f5b0ef32fc54d7578060e69636e
magicalfeyfenny <magicalfeyfenny@fenny.moe>
2025-11-19 21:50:16 -0800

    Added collision signals and signal handlers

--- Environment/Puzzles/SwitchButton.cs
+++ Environment/Puzzles/SwitchButton.cs
@@ -3,4 +3,22 @@ namespace CrossDimensions.Environment.Puzzles;
 public partial class SwitchButton : Node
 {
     public bool SwitchPressed { get; set; } = false;
+    private void OnArea2DBodyEntered(PhysicsBody2D body)
+    {
+        SwitchPressed = true;
+        SwitchDoor door = GetParentOrNull<SwitchDoor>();
+        if (door != null)
+        {
+            door.Activate();
+        }
+    }
+    private void OnArea2DBodyExited(PhysicsBody2D body)
+    {
+        SwitchPressed = false;
+        SwitchDoor door = GetParentOrNull<SwitchDoor>();
+        if (door != null)
+        {
+            door.Activate();
+        }
+    }
 }
