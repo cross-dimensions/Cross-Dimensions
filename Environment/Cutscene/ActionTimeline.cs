d12ae3e3ed5a889e471dc00e565c84359fd4c33f
magicalfeyfenny <magicalfeyfenny@fenny.moe>
2025-12-29 15:29:45 -0800

    comments and a couple other tweaks

--- Environment/Cutscene/ActionTimeline.cs
+++ Environment/Cutscene/ActionTimeline.cs
@@ -11,6 +11,8 @@ public class ActionTimeline
 {
     int TimelinePosition { get; set; }
     bool TimelineRunning { get; set; }
+    //dictionary contains key (timeline frame at which to execute) and string (path to GDScript)
+    //are we able to link GDScripts directly in the editor (which would be more ideal) if dictionary type is <int, GDscript>?
     Dictionary<int, string> TimelineMoments { get; set; }
 
     public void ExecuteTimelineStep() 
@@ -27,20 +29,22 @@ public class ActionTimeline
         TimelinePosition = 0;
     }
 
-    private void DoAtMoment( int position, string method ) 
+    private void DoAtMoment( int moment, string method ) 
     {
-        if (TimelineMoments.ContainsKey(position))
+        //if the moment exists, attempt to find and execute the associated GDScript
+        if (TimelineMoments.ContainsKey(moment))
         {
             try
             {
-                string path = TimelineMoments[position];
+                string path = TimelineMoments[moment];
                 GDScript script = GD.Load<GDScript>(path);
-                GodotObject node = (GodotObject)script.New();
-                node.Call(method);
+                GodotObject obj = (GodotObject)script.New();
+                obj.Call(method);
+                obj.Free();
             } 
             catch
             {
-                GD.PrintErr($"Timeline moment {position} could not be loaded!");
+                GD.PrintErr($"Timeline moment {moment} could not be loaded!");
             }
         }
     }
